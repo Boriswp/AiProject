a6a0d0d48a1306281aec9c8d7e08e23f530a9973
DESKTOP-RE00TG0\boris <boriswp99@gmail.com>
2022-05-12 00:28:56 +0500

    Update Bot.cs

--- Assets/Network/Gen/Bot.cs
+++ Assets/Network/Gen/Bot.cs
@@ -3,21 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 public class Bot : MonoBehaviour
 {
-    public float speed;//Speed Multiplier
-    public float rotation;//Rotation multiplier
-    public LayerMask raycastMask;//Mask for the sensors
+    public float speed;
+    public float rotation;
+    public LayerMask raycastMask;
 
-    private float[] input = new float[5];//input to the neural network
+    private float[] input = new float[5];
     public NeuralNetwork network;
 
     public int distance;
-
-    public float position = 1;//Checkpoint number on the course
-    public bool collided;//To tell if the car has crashed
+    
+    public float rewardPoints = 1;
+    public bool failed;
     public bool winner;
     public string collisionTag;
     public string checkpointTag;
@@ -25,27 +26,27 @@ public class Bot : MonoBehaviour
     private List<GameObject> checkedCheckPoints = new();
 
     
-    void FixedUpdate()//FixedUpdate is called at a constant interval
+    void FixedUpdate()
     {
-        if (collided) return;
-        for (var i = 0; i < 5; i++)//draws five debug rays as inputs
+        if (failed) return;
+        for (var i = 0; i < 5; i++)
         {
-            var newVector = Quaternion.AngleAxis(i * 45 - 90, new Vector3(0, 1, 0)) * transform.right;//calculating angle of raycast
+            var newVector = Quaternion.AngleAxis(i * 45 - 90, new Vector3(0, 1, 0)) * transform.right;
             var ray = new Ray(transform.position, newVector);
             if (Physics.Raycast(ray, out var hit,distance, raycastMask))
             {
-                input[i] = (distance - hit.distance) / distance;//return distance, 1 being close
+                input[i] = (distance - hit.distance) / distance;
             }
             else
             {
-                input[i] = 0;//if nothing is detected, will return 0 to network
+                input[i] = 0;
             }
         }
 
-        var output = network.FeedForward(input);//Call to network to feedforward
+        var output = network.FeedForward(input);
         
-        transform.Rotate(0, output[0] * rotation, 0, Space.World);//controls the cars movement
-        transform.position += transform.forward * output[1] * speed;//controls the cars turning
+        transform.Rotate(0, output[0] * rotation, 0, Space.World);
+        transform.position += transform.forward * output[1] * speed;
     }
 
 
@@ -53,24 +54,24 @@ public class Bot : MonoBehaviour
     {
         var collisionWithObject = other.gameObject;
       
-        if(collisionWithObject.CompareTag(checkpointTag))//check if the car passes a gate
+        if(collisionWithObject.CompareTag(checkpointTag))
         {
             var checkPoints = GameObject.FindGameObjectsWithTag(checkpointTag);
-            for (int i=0; i < checkPoints.Length; i++)
+            for (var i=0; i < checkPoints.Length; i++)
             {
                 if (collisionWithObject != checkPoints[i] || checkedCheckPoints.Contains(collisionWithObject)) continue;
                 checkedCheckPoints.Add(collisionWithObject);
-                position+=2;
+                rewardPoints+=2;
                 break;
             }
         }
         else if(collisionWithObject.CompareTag(collisionTag))
         {
-            position -=1;
-            collided = true;//stop operation if bot has collided
+            rewardPoints -=1;
+            failed = true;
         }else if(collisionWithObject.CompareTag("finish"))
         {
-            position += 100;
+            rewardPoints += 100;
             winner = true;
         }
     }
@@ -78,8 +79,8 @@ public class Bot : MonoBehaviour
 
     public void UpdateFitness()
     {
-        position+=Mathf.Sqrt(Vector3.Distance(GameObject.FindGameObjectWithTag("Start").transform.position, transform.position));
-        network.fitness = position;//updates fitness of network for sorting
+        rewardPoints+=Mathf.Sqrt(Vector3.Distance(GameObject.FindGameObjectWithTag("Start").transform.position, transform.position));
+        network.fitness = rewardPoints;
     }
 
     public void Save()
